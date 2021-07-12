9c436d768c0af9fd02675d21941cb2228219af66
Sajal Kaushal <sajalkaushal3@gmil.com>
2021-07-12 17:35:49 +0530

    app completed

--- app/src/main/java/com/example/geolocation/MainActivity.kt
+++ app/src/main/java/com/example/geolocation/MainActivity.kt
@@ -36,9 +36,7 @@ import java.util.*
 import java.util.jar.Manifest
 import kotlin.collections.ArrayList
 
- class MainActivity : AppCompatActivity(),OnMapReadyCallback
- //    , LocationListener, GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraMoveStartedListener, GoogleMap.OnCameraIdleListener
- {
+ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
      private var mMap :GoogleMap? = null
      lateinit var mapView:MapView
      private lateinit var search :Button
@@ -74,9 +72,6 @@ import kotlin.collections.ArrayList
              ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION),11)
          }
          mMap!!.isMyLocationEnabled = false
-//         mMap!!.setOnCameraMoveListener(this)
-//        mMap!!.setOnCameraMoveStartedListener(this)
-//        mMap!!.setOnCameraIdleListener(this)
      }
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -98,7 +93,7 @@ import kotlin.collections.ArrayList
          B_clear.setOnClickListener {
              mapView.onCreate(mapviewbundle)
              mapView.getMapAsync(this)
-
+             tvCurrentAddress.text = ""
          }
     }
       private fun remo(){
@@ -179,94 +174,11 @@ import kotlin.collections.ArrayList
 
                  } } } }
 
-     fun getDirectionURL(origin:LatLng, dest: LatLng) : String{
-         return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving"
-     }
-
-     private inner class GetDirection(val url : String) : AsyncTask<Void,Void,List<List<LatLng>>>(){
-         override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
-             val client = OkHttpClient()
-             val request = Request.Builder().url(url).build()
-             val response = client.newCall(request).execute()
-             val data = response.body()!!.string()
-             Log.d("GoogleMap" , " data : $data")
-             val result =  ArrayList<List<LatLng>>()
-             try{
-                 val respObj = Gson().fromJson(data,GoogleMapDTO::class.java)
-
-                 val path =  ArrayList<LatLng>()
-
-                 for (i in 0 until respObj.routes[0].legs[0].steps.size){
-//                    val startLatLng = LatLng(respObj.routes[0].legs[0].steps[i].start_location.lat.toDouble()
-//                            ,respObj.routes[0].legs[0].steps[i].start_location.lng.toDouble())
-//                    path.add(startLatLng)
-//                    val endLatLng = LatLng(respObj.routes[0].legs[0].steps[i].end_location.lat.toDouble()
-//                            ,respObj.routes[0].legs[0].steps[i].end_location.lng.toDouble())
-                     path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
-                 }
-                 result.add(path)
-             }catch (e:Exception){
-                 e.printStackTrace()
-             }
-             return result
-         }
-
-         override fun onPostExecute(result: List<List<LatLng>>) {
-             val lineoption = PolylineOptions()
-             for (i in result.indices){
-                 lineoption.addAll(result[i])
-                 lineoption.width(10f)
-                 lineoption.color(Color.BLUE)
-                 lineoption.geodesic(true)
-             }
-             mMap!!.addPolyline(lineoption)
-         }
-     }
-
-     public fun decodePolyline(encoded: String): List<LatLng> {
-
-         val poly = ArrayList<LatLng>()
-         var index = 0
-         val len = encoded.length
-         var lat = 0
-         var lng = 0
-
-         while (index < len) {
-             var b: Int
-             var shift = 0
-             var result = 0
-             do {
-                 b = encoded[index++].toInt() - 63
-                 result = result or (b and 0x1f shl shift)
-                 shift += 5
-             } while (b >= 0x20)
-             val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-             lat += dlat
-
-             shift = 0
-             result = 0
-             do {
-                 b = encoded[index++].toInt() - 63
-                 result = result or (b and 0x1f shl shift)
-                 shift += 5
-             } while (b >= 0x20)
-             val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
-             lng += dlng
-
-             val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
-             poly.add(latLng)
-         }
-
-         return poly
-     }
-
-
 
 
      override fun onSaveInstanceState(outState: Bundle) {
          super.onSaveInstanceState(outState)
-       //  askGalleryPermitionsLocation()
-         var mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
+          var mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
          if (mapViewBundle == null){
              mapViewBundle = Bundle()
              outState.putBundle(MAP_VIEW_BUNDLE_KEY,mapViewBundle)
